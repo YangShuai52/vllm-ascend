[MRv2][BugFix][310P] fix Qwen3.5 precision for MRv2 on the 310P
Signed-off-by: Thiagor2002 <13476117628@163.com>

vllm_ascend/_310p/quantization/methods/w8a8_dynamic.py
@@ -149,6 +149,7 @@ class AscendW8A8DynamicLinearMethod310(AscendW8A8Linear310pScheme):
         del tp_rank
         # NOTE(310P):
         # - There is an accuracy issue currently, which is expected to be fixed in the next version.
+        x = x.contiguous()
         quantized_x, pertoken_scale = torch_npu.npu_dynamic_quant(x)
         need_unsqz = False
         if pertoken_scale.dim() == 2:
@@ -156,8 +157,6 @@ class AscendW8A8DynamicLinearMethod310(AscendW8A8Linear310pScheme):
             quantized_x = quantized_x.squeeze(dim=1)
             pertoken_scale = pertoken_scale.squeeze(dim=1)
 
-        # NOTE(310P):
-        # - Currently, W8A8 dynamic quantization supports only symmetric quantization.
         output = torch_npu.npu_quant_matmul(
             quantized_x,
             layer.weight.data,
@@ -171,7 +170,6 @@ class AscendW8A8DynamicLinearMethod310(AscendW8A8Linear310pScheme):
         return output
 
     def process_weights_after_loading(self, layer: torch.nn.Module) -> None:
-        # cast quantized weight tensors in NZ format for higher inference speed
         layer.weight.data = maybe_trans_nz(layer.weight.data).transpose(0, 1)
         layer.weight_scale.data = layer.weight_scale.data.flatten()
         layer.weight_offset.data = layer.weight_offset.data.flatten()